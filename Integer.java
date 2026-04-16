0411ce0e65bced3170285c2a1594f6340a0c1fac
Balaganesh-s-m <balaganesh.s.011@gamil.com>
2026-04-16 15:35:14 +0530

    i can

--- Integer.java
+++ Integer.java
@@ -1,1 +1,14 @@
-hwll
+import java.util.*;
+public class Integer{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+
+
+        
+    }
+}
